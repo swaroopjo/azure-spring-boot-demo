8baeebfaa8e2f3074839739a71bba0767055cb41
Jyothiswaroop.Pamalla <swaroop_jo@yahoo.com>
2021-10-13 16:03:53 -0500

    move one folder up

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpServerErrorException;
 
 import java.util.Date;
 
@@ -9,8 +12,9 @@ import java.util.Date;
 public class HelloController {
 
 	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!" + new Date();
+	public ResponseEntity index() {
+
+		return new ResponseEntity("Some Internal Exception", HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 }
